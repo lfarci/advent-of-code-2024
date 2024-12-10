62e397e18a201d11995782d50b9654929d834d1c
Logan Farci <farci.logan@gmail.com>
2024-12-10 22:37:59 +0100

    Add dependency to the AdventOfCode.Kit.Console

---
+++ src/AdventOfCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode.Kit.Client.Models;
+using AdventOfCode.Kit.Console;
+using System.Runtime.CompilerServices;
+
+
+var console = AdventOfCodeConsole.Instance;
+
+
+console.StartYear(2024, year =>
+{
+    year.Submit<Day1>().ForDay(1);
+});
+
+class Day1 : Puzzle
+{
+    public override (Answer First, Answer Second) Run(string[] lines)
+    {
+        return (null, null);
+    }
+}
